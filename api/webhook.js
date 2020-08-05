2e401e2be6a076bda9878f8b6bf6cd3a482981b3
Fernando Daciuk <f.daciuk@gmail.com>
2020-08-05 17:31:02 -0300

    Change console.error to console.log

--- api/webhook.js
+++ api/webhook.js
@@ -28,7 +28,7 @@ const tweetIssue = (req) => {
   const { html_url: htmlUrl, title } = req.body.issue
   const tweet = { status: `${title}\n${htmlUrl}` }
   twitter.post('statuses/update', tweet, (err, tweet, response) => {
-    if (err) return console.error('Error posting on twitter:', err)
+    if (err) return console.log('Error posting on twitter:', err)
     console.log('Tweet ok!')
   })
   return tweet
@@ -41,7 +41,7 @@ const isProduction = (env) => (
 
 module.exports = (req, res) => {
   if (!isOpenedIssue(req) || !isGitHubSignatureValid(req)) {
-    console.log('error:', JSON.stringify(req.headers, null, 2))
+    console.log('error:', req.headers)
     throw new Error('It is not an opened issue or the signature is invalid')
   }
 
