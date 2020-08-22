56b1093e96895ce1660a0af2a96d1ecb1d29ae5d
Fernando Daciuk <f.daciuk@gmail.com>
2020-08-22 18:42:59 -0300

    Add first route and add PORT var

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const PORT = process.env.PORT || 8080
 
 const webhookRoute = require('./api/webhook')
 const tweetRoute = require('./api/tweet')
@@ -12,7 +13,8 @@ const tweetRoute = require('./api/tweet')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.get('/', (req, res) => res.send('GitHub 2 Twitter!'))
 app.post('/api/webhook', webhookRoute)
 app.get('/api/tweet', tweetRoute)
 
-app.listen(8080, () => console.log('Listening on localhost:8080'))
+app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`))
