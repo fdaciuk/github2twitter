6ffccbf5b118d640cca19ff0675b39b59b8a2173
Fernando Daciuk <f.daciuk@gmail.com>
2020-08-22 18:10:27 -0300

    Check if GitHub signature is valid

--- api/webhook.js
+++ api/webhook.js
@@ -41,7 +41,7 @@ const isProduction = (env) => (
 
 module.exports = (req, res) => {
   if (!isOpenedIssue(req) || !isGitHubSignatureValid(req)) {
-    console.log('error:', req.body.action)
+    console.log('isGitHubSignatureValid?', isGitHubSignatureValid(req))
     throw new Error('It is not an opened issue or the signature is invalid')
   }
 
